974e5c2f829e51a71de89882f7bc3d950cb35a52
Wujiazhang <163751835+Wujiazhang@users.noreply.github.com>
2025-05-22 19:14:54 +0800

    Add files via upload

---
+++ proposalbox.py
@@ -0,0 +1,48 @@
+from mmengine.runner import Runner
+from mmdet.apis import inference_detector, init_detector
+import mmcv
+from mmdet.structures import DetDataSample
+from mmengine.config import Config
+from mmengine.dataset import Compose
+import os
+import torch
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from mmcv.visualization import imshow_det_bboxes
+
+# 加载 config
+config_file = 'configs/mask_rcnn_r50_fpn_1x_coco2.py'
+checkpoint_file = 'work_dirs/mask_rcnn_r50_fpn_1x_coco2/epoch_100.pth'
+img_file = 'demo/in/2009_000759.jpg'
+
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+cfg = Config.fromfile(config_file)
+test_pipeline = Compose(cfg.test_dataloader.dataset.pipeline)
+
+data = dict(img_path=img_file, img_id=0)
+data = test_pipeline(data)
+data['inputs'] = data['inputs'].unsqueeze(0)
+data['data_samples'] = [data['data_samples']]
+data_processed = model.data_preprocessor(data, False)
+
+with torch.no_grad():
+    features = model.extract_feat(data_processed['inputs'])
+    proposals = model.rpn_head.predict(features, data_processed['data_samples'], rescale=False)
+    scores = proposals[0].scores.cpu().numpy()
+    bboxes = proposals[0].bboxes.cpu().numpy()
+    labels = proposals[0].labels.cpu().numpy()
+
+img_model = data['inputs'].squeeze(0).permute(1,2,0).cpu().numpy()
+
+imshow_det_bboxes(
+ img_model,
+ bboxes[:10],
+ scores[:10],
+ show=False,
+ out_file='outputs/inproposal/2009_000759.jpg'
+)
+# mmcv.imwrite(img_model, 'outputs/inproposal/2007_000822.jpg')
+#2008_000009.jpg 2009_000628.jpg 2009_000759.jpg
+# python proposalbox.py
